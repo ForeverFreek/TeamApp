2bad7a00ea86af64bb058f5412e4019e70dd921f
Armin Woerner <wolf.woerner@freenet.de>
2017-05-17 21:42:34 +0200

    Added in the app id and a new basic activity.

---
+++ app/src/main/java/de/pfaffenhofen/mtv/teamapp/FbLoginActivity.java
@@ -0,0 +1,13 @@
+package de.pfaffenhofen.mtv.teamapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FbLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fb_login);
+    }
+}
